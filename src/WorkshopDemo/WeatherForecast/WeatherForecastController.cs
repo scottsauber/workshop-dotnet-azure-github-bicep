6b1fe95e3eacaf9f02b17a00634237fe56c66dc6
Scott Sauber <stsauber@gmail.com>
2024-11-17 17:29:29 -0500

    add slow endpoint

--- src/WorkshopDemo/WeatherForecast/WeatherForecastController.cs
+++ src/WorkshopDemo/WeatherForecast/WeatherForecastController.cs
@@ -23,5 +23,26 @@ public class WeatherForecastController
             .ToArray();
         return forecasts;
     }
-    
+
+ 
+    [HttpGet("slow")]
+    public async Task<IEnumerable<WeatherForecast>> GetSlow()
+    {
+        var randomWaitInMs = Random.Shared.Next(500, 5000);
+        await Task.Delay(randomWaitInMs);
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        var forecasts =  Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecasts;
+    }   
 }
