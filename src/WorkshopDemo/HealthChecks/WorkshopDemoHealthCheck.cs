9996a546635e2651d3c73d3e4504f8f7a4b1e574
Scott Sauber <stsauber@gmail.com>
2024-11-18 15:16:35 -0500

    something went boom

--- src/WorkshopDemo/HealthChecks/WorkshopDemoHealthCheck.cs
+++ src/WorkshopDemo/HealthChecks/WorkshopDemoHealthCheck.cs
@@ -6,6 +6,6 @@ public class WorkshopDemoHealthCheck : IHealthCheck
 {
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        return Task.FromResult(HealthCheckResult.Healthy("A healthy result."));
+        return Task.FromResult(HealthCheckResult.Unhealthy("Something went boom"));
     }
 }
