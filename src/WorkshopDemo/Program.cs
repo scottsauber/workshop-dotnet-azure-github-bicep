7c7a5d8cf86d6d57f4b3e719a64301d6ef5736d9
Scott Sauber <stsauber@gmail.com>
2024-11-17 20:21:16 -0500

    revert program back

--- src/WorkshopDemo/Program.cs
+++ src/WorkshopDemo/Program.cs
@@ -8,71 +8,27 @@ using WorkshopDemo.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .CreateBootstrapLogger();
+// Add services to the container.    
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddHealthChecks()
+    .AddCheck<WorkshopDemoHealthCheck>(nameof(WorkshopDemoHealthCheck));
+builder.Services.AddSingleton<IFileService, FileService>();
+builder.Services.AddSingleton<IVersionService, VersionService>();
 
-try
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    Log.Information("Starting web host");
-    
-    // Add services to the container.
-    builder.Host.UseSerilog((ctx, lc) => lc
-        .MinimumLevel.Information()
-        .MinimumLevel.Override("WorkshopDemo", LogEventLevel.Debug)
-        .MinimumLevel.Override("System", LogEventLevel.Error)
-        .WriteTo.Console(
-            outputTemplate:
-            "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
-            theme: AnsiConsoleTheme.Literate)
-        .WriteTo.ApplicationInsights(
-            new TelemetryConfiguration
-                { ConnectionString = ctx.Configuration.GetConnectionString("ApplicationInsights") },
-            TelemetryConverter.Traces)
-        .Enrich.FromLogContext());
-    
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddOpenApi();
-    builder.Services.AddControllers();
-    builder.Services.AddHealthChecks()
-        .AddCheck<WorkshopDemoHealthCheck>(nameof(WorkshopDemoHealthCheck));
-    builder.Services.AddSingleton<IFileService, FileService>();
-    builder.Services.AddSingleton<IVersionService, VersionService>();
-
-    builder.Configuration.AddAzureKeyVault(
-        new Uri($"https://kv-scottsauber-{builder.Environment.EnvironmentName}.vault.azure.net/"),
-        new ChainedTokenCredential(new AzureCliCredential(), new ManagedIdentityCredential()));
-
-    builder.Services.AddApplicationInsightsTelemetry(options =>
-    {
-        options.ConnectionString = builder.Configuration.GetConnectionString("ApplicationInsights");
-    });
-
-    var app = builder.Build();
-
-    app.UseSerilogRequestLogging();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.MapOpenApi();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.MapHealthChecks("/api/healthz");
-
-    app.MapControllers();
-
-    app.Run();
+    app.MapOpenApi();
 }
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Application terminated");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
+
+app.UseHttpsRedirection();
+
+app.MapHealthChecks("/api/healthz");
+
+app.MapControllers();
+
+app.Run();
